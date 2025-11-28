c8fbc52566ad65ed4c625cbe102d6a47948a6ab6
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-28 23:26:26 +0000

    Allow building packages and a VM image

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
 {
+  pkgs ? import (import ./lon.nix).nixpkgs { },
   nixos ? import "${(import ./lon.nix).nixpkgs}/nixos" { },
 }:
-nixos.config.system.build.isoImage
+{
+  iso = nixos.config.system.build.isoImage;
+  vm = nixos.vm;
+
+  cosmic-ext-alternative-startup =
+    pkgs.callPackage ./pkgs/cosmic-ext-alternative-startup/package.nix
+      { };
+
+  cosmic-ext-extra-sessions = pkgs.callPackage ./pkgs/cosmic-ext-extra-sessions/package.nix { };
+}
